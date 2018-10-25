993dd855d35a3dabc6551a90c1dfe64269cbbd70
stango <55555.tango@gmail.com>
2018-10-25 18:55:20 +0300

    first commit

---
+++ app/src/androidTest/java/com/sqisland/espresso/toolbartitle/MainActivityTest.kt
@@ -0,0 +1,28 @@
+package com.sqisland.espresso.toolbartitle
+
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.matcher.ViewMatchers.*
+import android.support.test.rule.ActivityTestRule
+import android.support.test.runner.AndroidJUnit4
+import android.widget.TextView
+import android.support.v7.widget.Toolbar
+import org.hamcrest.CoreMatchers.allOf
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+    @Rule @JvmField
+    var activityRule = ActivityTestRule(MainActivity::class.java)
+
+    @Test
+    fun toolbarTitle() {
+        onView(
+            allOf(
+                isAssignableFrom(TextView::class.java),
+                withParent(isAssignableFrom(Toolbar::class.java))))
+            .check(matches(withText(R.string.title)))
+    }
+}
