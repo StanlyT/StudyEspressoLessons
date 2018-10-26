c07080b10b057e984ba9ba7868111da1ee4223aa
stango <55555.tango@gmail.com>
2018-10-26 14:47:01 +0300

    first commit

--- app/src/androidTest/java/com/sqisland/espresso/toolbartitle/MainActivityTest.kt
+++ app/src/androidTest/java/com/sqisland/espresso/toolbartitle/MainActivityTest.kt
@@ -1,18 +1,24 @@
 package com.sqisland.espresso.toolbartitle
 
+import android.support.test.InstrumentationRegistry
 import android.support.test.espresso.Espresso.onView
 import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.matcher.BoundedMatcher
 import android.support.test.espresso.matcher.ViewMatchers.*
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
 import android.widget.TextView
 import android.support.v7.widget.Toolbar
+import android.view.View
 import org.hamcrest.CoreMatchers.allOf
+import org.hamcrest.Description
+import org.hamcrest.Matcher
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
+
 class MainActivityTest {
     @Rule @JvmField
     var activityRule = ActivityTestRule(MainActivity::class.java)
